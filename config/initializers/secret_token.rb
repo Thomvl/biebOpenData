d4a7ffeab1026618b3e003a9a4971666c846db7a
Thom van Ledden <tvledden@gmail.com>
2011-04-05 13:42:28 +0200

    Clean RoR Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BiebOpenData::Application.config.secret_token = '2ab2a48015788b385e545faa6abe9697240f61ea763876cfdf73ea09320e9b750e1348232fe4b747299ac13b06f2185b719f96c4e1cbed1d74d113912c299b93'
